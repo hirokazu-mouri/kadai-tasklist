f37f19e9c614a203445be4852c615eb636b6c108
Hirokazu Mori <hirokazu.mouri@gmail.com>
2017-08-22 08:36:50 +0000

    CRUD tasks

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,11 @@
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
 });
+*/
+
+Route::get('/', 'tasksController@index');
+
+Route::resource('tasks', 'tasksController');
