76c2bdbff528f011326be73166980f74ff9bf439
Hirokazu Mori <hirokazu.mouri@gmail.com>
2017-09-12 09:15:05 +0000

    login

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -22,6 +22,8 @@ class AuthController extends Controller
     */
 
     use AuthenticatesAndRegistersUsers, ThrottlesLogins;
+    
+        protected $loginPath = '/login';    // 追加
 
     /**
      * Create a new authentication controller instance.
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,3 +19,8 @@
 Route::get('/', 'tasksController@index');
 
 Route::resource('tasks', 'tasksController');
+
+// ログイン認証
+Route::get('login', 'Auth\AuthController@getLogin')->name('login.get');
+Route::post('login', 'Auth\AuthController@postLogin')->name('login.post');
+Route::get('logout', 'Auth\AuthController@getLogout')->name('logout.get');
